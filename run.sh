a0a51907bfcea3c0be6da22bf754ad45c9483b4f
zyj <zhaoyj2370@163.com>
2024-07-03 00:44:37 +0800

    构建脚本

--- run.sh
+++ run.sh
@@ -7,4 +7,7 @@
 #!/bin/zsh
 PATH=/home/edison/bin:/home/edison/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
 export PATH
+echo "清理生成"
+cmake --build build/ --target clean
+echo "开始构建"
 cmake -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
