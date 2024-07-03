208e8e4bea913a5469fbc5212c66681b1d77fe4e
zyj <zhaoyj2370@163.com>
2024-07-03 11:33:45 +0800

    构建脚本

--- generate.sh
+++ generate.sh
@@ -7,10 +7,9 @@
 #!/bin/zsh
 PATH=/home/edison/bin:/home/edison/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/work/tools/gcc-3.4.5-glibc-2.3.6/bin
 export PATH
-
-echo "开始编译..."
-cd build
-cores=${nprpc}
-ninja -t clean && ninja -j "$cores" $1
-echo "开始运行。。。"
-./app/$1
+echo "清理生成"
+cmake --build build/ --target clean
+rm -rf build
+mkdir build
+echo "开始构建"
+cmake -G Ninja -S . -B build  -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++
